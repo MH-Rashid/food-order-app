396b31aa283a6435dad58b5913ecfcd66a87fd0b
MoRashid <MoRashid>
2024-01-27 10:52:37 +0000

    test

--- src/http.js
+++ src/http.js
@@ -1,13 +1,10 @@
-import mealsData from '../assets/available-meals.json'
-
 export async function fetchAvailableMeals() {
-//  const response = await fetch("https://arif-food-order-app.netlify.app/assets/available-meals.json");
+  const response = await fetch("http://localhost:3000/meals");
   
-//  if (!response.ok) {
-//    throw new Error("Failed to fetch meals. Please try again later");
-//  }
+  if (!response.ok) {
+    throw new Error("Failed to fetch meals. Please try again later");
+  }
   
-//  const resData = await response.json();
-//  return resData;
-  return mealsData;
+  const resData = await response.json();
+  return resData;
 }
