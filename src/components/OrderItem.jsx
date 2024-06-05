c40b8a8a406ba2326d985f436a8ac82ab756b047
MoRashid <MoRashid>
2024-06-05 15:56:28 +0100

    Made reoder button functional

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,4 +1,16 @@
+import { useContext } from "react";
+import { CartContext } from "../store/meal-cart-context.jsx";
+
 export default function OrderItem({ orderDetails }) {
+  const { addItem } = useContext(CartContext);
+
+  function handleReorder() {
+    const { items } = orderDetails;
+    items.forEach((item) => {
+      addItem(item);
+    });
+  }
+
   return (
     <li className="order-item">
       <div>
@@ -13,7 +25,9 @@ export default function OrderItem({ orderDetails }) {
           ))}
         </ul>
       </div>
-      <button className="orange-button">Reorder</button>
+      <button className="orange-button" onClick={handleReorder}>
+        Reorder
+      </button>
     </li>
   );
 }
