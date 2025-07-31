dc8fe712f7fcb10f19ff96895f0f5b9bb0568601
MH-Rashid <morashid99@hotmail.com>
2025-07-31 20:23:44 +0100

    set up and connect to MongoDB

---
+++ dbconfig.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+module.exports = connectDB;
