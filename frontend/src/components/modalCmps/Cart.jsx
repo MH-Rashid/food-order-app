f82dfc2efdecea22588174c31948afe511f673e7
MH-Rashid <morashid99@hotmail.com>
2025-08-16 23:52:05 +0100

    resolve Edge issue with reading totalPrice when loading cart

--- frontend/src/components/modalCmps/Cart.jsx
+++ frontend/src/components/modalCmps/Cart.jsx
@@ -7,10 +7,11 @@ export default function Cart({ onCheckout }) {
   const { items, updateItem, resetCart } = useContext(AppContext);
 
   const totalPrice = Array.isArray(items)
-    ? items?.reduce(
-        (accumulator, item) => accumulator + item.price * item.quantity,
-        0
-      )
+    ? items.reduce((acc, item) => {
+        const price = Number(item?.price) || 0;
+        const quantity = Number(item?.quantity) || 0;
+        return acc + price * quantity;
+      }, 0)
     : 0;
 
   const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
