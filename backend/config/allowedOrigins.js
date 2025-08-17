4feb79ca71342bc8c2882d0dc76e5359a43a5e7c
MH-Rashid <morashid99@hotmail.com>
2025-08-17 23:58:17 +0100

    deployment fix

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://www.google.com", // to access the site via Google search results
   "http://127.0.0.1:5500",
-  "https://food-order-app-fullstack-kft7o385j-mh-rashids-projects.vercel.app",
+  "https://food-order-app-fullstack-32wppvlig-mh-rashids-projects.vercel.app",
 ];
 
 module.exports = allowedOrigins;
