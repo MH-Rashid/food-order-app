37983b522df0373087762a0ddaed2abe019b0aef
MH-Rashid <morashid99@hotmail.com>
2025-09-04 20:53:07 +0100

    updated allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://www.google.com", // to access the site via Google search results
   "http://127.0.0.1:5500",
+  "http://localhost:5173",
   "https://food-order-app-fullstack-32wppvlig-mh-rashids-projects.vercel.app",
 ];
 
